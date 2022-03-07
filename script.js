27efd20c710cbcd2420788fa597ac3ee4063a9e6
kidcodermaht <walkthroughfaya@gmail.com>
2022-03-07 21:38:33 +0330

    changed key

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ function handleSubmit(event) {
 }
 
 function searchLocation(position) {
-  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiKey = "b761a023f32606217de81af7721e0f32";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(displayWeatherCondition);
