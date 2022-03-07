cfe69fb4e9c3550bca509e840d9193f4df1da3f1
kidcodermaht <walkthroughfaya@gmail.com>
2022-03-07 14:32:22 +0330

    fixed js :(

--- script.js
+++ script.js
@@ -61,14 +61,6 @@ function getCurrentLocation(event) {
   navigator.geolocation.getCurrentPosition(searchLocation);
 }
 
-function search(event) {
-  event.preventDefault();
-  let cityElement = document.querySelector("#city");
-  let cityInput = document.querySelector("#city-input");
-  cityElement.innerHTML = cityInput.value;
-  s;
-}
-
 function convertToFahrenheit(event) {
   event.preventDefault();
   let temperatureElement = document.querySelector("#temperature");
@@ -81,45 +73,12 @@ function convertToCelsius(event) {
   temperatureElement.innerHTML = 19;
 }
 
-// Feature #1
 let dateElement = document.querySelector("#date");
 let currentTime = new Date();
 dateElement.innerHTML = formatDate(currentTime);
 
-// Feature #2
 let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", search);
-
-// Bonus Feature
-let fahrenheitLink = document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click", convertToFahrenheit);
-
-let celsiusLink = document.querySelector("#celsius-link");
-celsiusLink.addEventListener("click", convertToCelsius);
-
-let apiKey = "b761a023f32606217de81af7721e0f32";
-let city = "tehran";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city} &units=metric`;
-
-function showTemperature(response) {
-  let temperature = Math.round(response.data.main.temp);
-  let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = temperature;
-}
-let h1 = document.querySelector("#city");
-h1.innerHTML = city;
-
-axios.get(`${apiUrl}&appid=${apiKey}`).then(showTemperature);
-
-function showPosition(position) {
-  let h1 = document.querySelector("h1");
-  h1.innerHTML = `Your Latitude is ${position.coords.latitude} and your longitude is ${position.coords.longitude}`;
-}
-
-
-
-let button = document.querySelector("button");
-button.addEventListener("click", getCurrentPosition);
+searchForm.addEventListener("submit", handleSubmit);
 
 let currentLocationButton = document.querySelector("#current-location-button");
 currentLocationButton.addEventListener("click", getCurrentLocation);
