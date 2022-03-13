90d72602c5684f33c50b24bf1ee5ec0a009539b1
kidcodermaht <walkthroughfaya@gmail.com>
2022-03-13 21:36:53 +0330

    added icons

--- script.js
+++ script.js
@@ -33,6 +33,15 @@ function displayWeatherCondition(response) {
   );
   document.querySelector("#description").innerHTML =
     response.data.weather[0].main;
+  document
+    .querySelector("#icon")
+    .setAttribute(
+      "src",
+      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
+  document
+    .querySelector("img")
+    .setAttribute("alt", response.data.weather[0].description);
 }
 function searchCity(city) {
   let apiKey = "b761a023f32606217de81af7721e0f32";
